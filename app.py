ab9b60c2c1d94c68eea5534a489e567ba1438f31
Shahnaz Umer <shahnazumer@hotmail.com>
2024-05-29 00:20:24 +0500

    initialize langfuse

--- app.py
+++ app.py
@@ -10,9 +10,9 @@ os.environ["OPENAI_API_KEY"] = ""
  # Initialize Langfuse handler
 from langfuse.callback import CallbackHandler
 langfuse_handler = CallbackHandler(
-    public_key="pk-lf-42cca64b-94cf-4e3f-b81e-c1895c0f5296",
-    secret_key="sk-lf-a717f91d-acbd-4b22-97f2-abcf9745e89f",
-    host="http://ec2-18-212-80-177.compute-1.amazonaws.com:3000"
+    public_key="",
+    secret_key="",
+    host="http://:3000"
 )
 
 prompt1 = ChatPromptTemplate.from_template("what is the city {person} is from?")
